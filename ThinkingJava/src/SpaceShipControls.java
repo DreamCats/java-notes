6aad34102c074b5fd0247f27a4eb1df4f09c1bf4
DreamCats <maifeng_cat@outlook.com>
2019-03-10 11:02:53 +0800

    commit SpaceShipControls

---
+++ ThinkingJava/src/SpaceShipControls.java
@@ -0,0 +1,16 @@
+/**
+ * @program JavaBooks
+ * @description: 代理-控制器
+ * @author: mf
+ * @create: 2019/03/10 11:02
+ */
+
+public class SpaceShipControls {
+    void up(int velocity) {}
+    void down(int velocity) {}
+    void left(int velocity) {}
+    void right(int velocity) {}
+    void forward(int velocity) {}
+    void back(int velocity) {}
+    void turboBoost() {}
+}
