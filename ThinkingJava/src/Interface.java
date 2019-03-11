bf27f9000c7bcd2d2f071c8c6d6fd87c8def6d57
DreamCats <maifeng_cat@outlook.com>
2019-03-11 19:47:03 +0800

    commit 接口和抽象之间的区别

--- ThinkingJava/src/Interface.java
+++ ThinkingJava/src/Interface.java
@@ -57,3 +57,39 @@ interface Door {
     public abstract void open();
     public abstract void close();
 }*/
+
+// 假如我们需要一个报警的功能。。alarm( )
+
+// 1。将这三个功能都放在抽象类里面，但是这样一来所有继承于这个抽象类的子类都具备了报警功能，但是有的门并不一定具备报警功能；
+// 2。将这三个功能都放在接口里面，需要用到报警功能的类就需要实现这个接口中的open( )和close( )，也许这个类根本就不具备open( )和close( )这两个功能，比如火灾报警器。
+
+/*
+interface Alarm {
+    void alarm();
+}
+
+abstract class Door {
+
+    void open();
+    void close();
+
+}
+
+class AlarmDoor extends Door implements Alarm {
+    void oepn() {
+        //....
+    }
+    void close() {
+        //....
+    }
+    void alarm() {
+        //....
+    }
+}*/
+
+// Door的open() 、close()和alarm()根本就属于两个不同范畴内的行为
+// open()和close()属于门本身固有的行为特性
+// alarm()属于延伸的附加行为
+// 单独将报警设计为一个接口
+// 包含alarm()行为,Door设计为单独的一个抽象类，包含open和close两种行为
+// 再设计一个报警门继承Door类和实现Alarm接口。
