84b6f2f7b349b53e22f549aeb3320b259fedb0b0
DreamCats <maifeng_cat@outlook.com>
2019-03-11 16:47:53 +0800

    commit interface 接口

---
+++ ThinkingJava/src/Interface.java
@@ -0,0 +1,12 @@
+/**
+ * @program JavaBooks
+ * @description: 接口
+ * @author: mf
+ * @create: 2019/03/11 16:45
+ */
+
+public interface Interface {
+}
+
+// 在软件工程中，接口泛指供别人调用的方法或者函数
+// 在这里， 它是对行为的抽象
