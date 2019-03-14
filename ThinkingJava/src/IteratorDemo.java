8037fa52609236683394920907340b24fd6c9563
DreamCats <maifeng_cat@outlook.com>
2019-03-14 15:33:52 +0800

    commit iterator first array

---
+++ ThinkingJava/src/IteratorDemo.java
@@ -0,0 +1,18 @@
+/**
+ * @program JavaBooks
+ * @description: 迭代器
+ * @author: mf
+ * @create: 2019/03/14 15:30
+ */
+
+public class IteratorDemo {
+
+    public static void main(String[] args){
+        // 对于数组
+
+        int array[] = new int[3];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
