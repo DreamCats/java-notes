0e94c866ae5d1c8e793e8ea8725abedfbd40084a
DreamCats <maifeng_cat@outlook.com>
2019-03-14 16:18:04 +0800

    commit Map 遍历

--- ThinkingJava/src/MapDemo.java
+++ ThinkingJava/src/MapDemo.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -26,5 +27,43 @@ public class MapDemo {
         // 清空
         map.clear();
 
+        // 遍历
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+
+        // 使用keySet遍历
+        System.out.println("使用keySet遍历");
+        for (String s : map.keySet()) {
+            System.out.println(s + ":" + map.get(s));
+        }
+
+        // 使用entrySet遍历
+        System.out.println("使用entrySet遍历");
+        for (Map.Entry<String, String> stringStringEntry : map.entrySet()) {
+            System.out.println(stringStringEntry.getKey() + ":" + stringStringEntry.getValue());
+        }
+
+
+        // 使用迭代器遍历
+        System.out.println("使用迭代器遍历---keySet");
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            System.out.println(key + ":" + map.get(key));
+        }
+
+        // 使用迭代器遍历
+        System.out.println("使用迭代器遍历---entrySet");
+
+        Iterator<Map.Entry<String, String>> iterator1 = map.entrySet().iterator();
+
+        while (iterator1.hasNext()) {
+            Map.Entry<String, String> entry = iterator1.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        // 增强for循环使用方便，但性能较差，不适合处理超大量级的数据。
+        // 迭代器的遍历速度要比增强for循环快很多，是增强for循环的2倍左右。
+        // 使用entrySet遍历的速度要比keySet快很多，是keySet的1.5倍左右。
     }
 }
