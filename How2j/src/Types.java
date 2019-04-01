0e13c7d89f7df96aa25cdeca55705014d79db855
DreamCats <maifeng_cat@outlook.com>
2019-04-01 10:25:53 +0800

    commit 4种整形数据

--- How2j/src/Types.java
+++ How2j/src/Types.java
@@ -6,4 +6,17 @@
  */
 
 public class Types {
+    public static void main(String[] args) {
+
+        // 整形数据
+        byte b = 1;    //8位  -128~127
+        short s= 200;   //16位 -32768~32767
+        int i = 300;    //32位
+        long l = 400;   // 64位
+
+
+        // if we assgin b to 300;
+//        b = 300; // 此时编译器就报错
+
+    }
 }
