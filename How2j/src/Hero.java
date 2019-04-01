5342fc705f2bc23ca13972b1995833888692228a
DreamCats <maifeng_cat@outlook.com>
2019-04-01 09:56:34 +0800

    commit 创建英雄对象

--- How2j/src/Hero.java
+++ How2j/src/Hero.java
@@ -11,5 +11,19 @@ public class Hero {
     float armor; // 护甲
     int moveSpeed; // 移动速度
 
+    public static void main(String[] args) {
+        Hero garen = new Hero();
+        garen.name = "盖伦";
+        garen.hp = 616.28f;
+        garen.armor = 27.536f;
+        garen.moveSpeed = 35;
+
+        Hero teemo = new Hero();
+        teemo.name = "提莫";
+        teemo.hp = 383f;
+        teemo.armor = 14f;
+        teemo.moveSpeed = 330;
+
+    }
 }
 
