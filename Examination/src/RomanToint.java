ef0a0f6131df6b2a470f9396cd7a14d40fc270e9
DreamCats <maifeng_cat@outlook.com>
2019-04-09 11:23:41 +0800

    commit 罗马数字转数字

---
+++ Examination/src/RomanToint.java
@@ -0,0 +1,21 @@
+/**
+ * @program JavaBooks
+ * @description: 罗马数字转数字
+ * @author: mf
+ * @create: 2019/04/09 11:16
+ */
+
+public class RomanToint {
+    public static void main(String[] args) {
+        SolutionRomanToint s = new SolutionRomanToint();
+        int res = s.romanToInt("MCMXCIV");
+
+    }
+}
+
+
+class SolutionRomanToint {
+    public int romanToInt(String s) {
+
+    }
+}
