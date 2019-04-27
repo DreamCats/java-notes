85d7592e7d63f4033cf636a52cbdb7fd364b6396
DreamCats <maifeng_cat@outlook.com>
2019-04-27 13:27:44 +0800

    commit 实现StrStr方法

---
+++ Examination/src/StrStr.java
@@ -0,0 +1,46 @@
+/**
+ * @program JavaBooks
+ * @description: 实现StrStr方法
+ * @author: mf
+ * @create: 2019/04/27 13:11
+ */
+
+public class StrStr {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "lo";
+        SolutionStrStr solutionStrStr = new SolutionStrStr();
+        int res = solutionStrStr.strStr(haystack, needle);
+        System.out.println(res);
+    }
+}
+
+class SolutionStrStr {
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() == needle.length()) {
+            if (haystack == needle) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            int k = i;
+            int j = 0;
+            while (j < needle.length() && k < haystack.length() && haystack.charAt(k) == needle.charAt(j)) {
+                j += 1;
+                k += 1;
+                if (j == needle.length()) {
+                    return i;
+                }
+            }
+
+        }
+        if (needle == null) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
