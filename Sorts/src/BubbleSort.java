642fcbff626b4349d6fd7b2fad4b3a093dd69dd6
DreamCats <maifeng_cat@outlook.com>
2019-08-11 17:08:48 +0800

    冒泡排序

--- Sorts/src/BubbleSort.java
+++ Sorts/src/BubbleSort.java
@@ -28,16 +28,11 @@ public class BubbleSort {
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
         for (int i = 1; i < arr.length; i++) {
             // 设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
-            boolean flag = true;
             for (int j = 0; j < arr.length - i; j++) {
                 // 如果前面的数比后面的数大，则交换
                 if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j+1);
-                    flag = false;
+                    swap(arr, j, j + 1);
                 }
-            }
-            if (flag) {
-                break;
             }
         }
         return arr;
