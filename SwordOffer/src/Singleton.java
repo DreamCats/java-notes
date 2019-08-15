60ff2ba3ba4510995397090dbd9adee92ffc0e74
DreamCats <maifeng_cat@outlook.com>
2019-08-15 16:41:18 +0800

    单例模式

---
+++ SwordOffer/src/Singleton.java
@@ -0,0 +1,87 @@
+/**
+ * @program JavaBooks
+ * @description: 单例模式
+ * @author: mf
+ * @create: 2019/08/15 15:53
+ */
+
+/*
+饿汉
+ */
+public class Singleton {
+    private static Singleton instance = new Singleton();
+    private Singleton(){}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+
+/*
+饿汉变种
+ */
+class Singleton1 {
+    private static Singleton1 instance = null;
+    static {
+        instance = new Singleton1();
+    }
+    private Singleton1() {}
+
+    public static Singleton1 getInstance() {
+        return instance;
+    }
+}
+
+/*
+懒汉 -- 线程不安全...
+ */
+class Singleton2 {
+    private static Singleton2 instance = null;
+    private Singleton2(){}
+
+    public static Singleton2 getInstance() {
+        if (instance == null){
+            instance = new Singleton2();
+        }
+        return instance;
+
+    }
+}
+
+/*
+懒汉 -- 线程安全， 但消耗资源较为严重
+ */
+class Singleton3 {
+    private static Singleton3 instance = null;
+
+    private Singleton3() {
+    }
+
+    public static synchronized Singleton3 getInstance() {
+        if (instance == null) {
+            instance = new Singleton3();
+        }
+        return instance;
+    }
+}
+
+/*
+懒汉 -- 线程安全，相对上一种好一些，双重校验
+ */
+class Singleton4 {
+    private static volatile Singleton4 instance = null;
+
+    private Singleton4() {
+    }
+
+    public static Singleton4 getInstance() {
+        if (instance == null) {
+            synchronized (Singleton4.class) {
+                if (instance == null) {
+                    instance = new Singleton4();
+                }
+            }
+        }
+        return instance;
+    }
+}
