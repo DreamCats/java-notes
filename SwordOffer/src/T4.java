abc4b68c92221f6a21f2883a0f604a61b3d2ee6e
DreamCats <maifeng_cat@outlook.com>
2019-08-17 15:47:23 +0800

    二维数组中的查找

--- SwordOffer/src/T4.java
+++ SwordOffer/src/T4.java
@@ -18,6 +18,20 @@ public class T4 {
                        {2, 4, 9, 12},
                        {4, 7, 10, 13},
                        {6, 8, 11, 15}};
+        boolean res = find(arr, 7);
+        System.out.println(res);
+    }
 
+    public static boolean find(int[][] arr, int target) {
+        int row = 0;
+        // i = 最后一行，往上扫
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // 往右扫
+            // 只要小于target，row就++，不满足条件则跳到上一行，保持现在的row
+            while (row < arr[0].length && arr[i][row] < target) row++;
+            if (row == arr[0].length) return false;// row 越界 flase
+            if(arr[i][row] == target) return true; // 判断是否相等
+        }
+        return false;
     }
 }
