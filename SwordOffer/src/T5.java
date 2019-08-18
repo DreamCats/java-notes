d5dc0dd0f4e88627c137fddeabb13d9dba3e9658
DreamCats <maifeng_cat@outlook.com>
2019-08-18 18:40:31 +0800

    替换空格

---
+++ SwordOffer/src/T5.java
@@ -0,0 +1,13 @@
+/**
+ * @program JavaBooks
+ * @description: 替换空格
+ * @author: mf
+ * @create: 2019/08/18 18:27
+ */
+
+/*
+请实现一个函数，把字符串中的每个空格替换成"%20"，例如
+输入"We are happy."，则输出"We%20are%20happy."
+ */
+public class T5 {
+}
