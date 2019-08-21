d03e7b41d04b535854d6dcc3cde72cc6f9a25a7a
DreamCats <maifeng_cat@outlook.com>
2019-08-21 14:20:32 +0800

    用两个栈实现一个队列

---
+++ SwordOffer/src/T8.java
@@ -0,0 +1,14 @@
+/**
+ * @program JavaBooks
+ * @description: 用两个栈实现队列
+ * @author: mf
+ * @create: 2019/08/21 14:18
+ */
+
+/*
+用两个栈实现一个队列。队列的声明如下，请实现它的两个函数
+appendTail和deleteHead，分别完成在队列尾部插入节点和队列
+头部删除节点的功能。
+ */
+public class T8 {
+}
