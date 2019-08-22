c5c72ae58c257f2df37b48ee33e0fdbb215cd0d4
DreamCats <maifeng_cat@outlook.com>
2019-08-22 14:25:59 +0800

    斐波那契数列

---
+++ SwordOffer/src/T9.java
@@ -0,0 +1,17 @@
+/**
+ * @program JavaBooks
+ * @description: 斐波那契数列
+ * @author: mf
+ * @create: 2019/08/22 14:22
+ */
+
+/*
+求斐波那契数列的第n项
+写一个函数，输入n，求斐波那契数列的第n项，
+斐波那契数列的定义如下：
+        0             n=0
+f(n)=   1             n=1
+        f(n-1)+f(n-2) n>1
+ */
+public class T9 {
+}
