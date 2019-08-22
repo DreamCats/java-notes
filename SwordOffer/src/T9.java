bd6ece41866fb3e7a1442fe5df318b89f3b2e589
DreamCats <maifeng_cat@outlook.com>
2019-08-22 15:02:33 +0800

    斐波那契数列

--- SwordOffer/src/T9.java
+++ SwordOffer/src/T9.java
@@ -14,4 +14,32 @@ f(n)=   1             n=1
         f(n-1)+f(n-2) n>1
  */
 public class T9 {
+    public static void main(String[] args) {
+        int res = fibonacci(10);
+        System.out.println(res);
+        int res1 = fibonacci2(10);
+        System.out.println(res1);
+    }
+    // 递归 缺点太慢
+    public static int fibonacci(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    // 从下向上循环
+    public static int fibonacci2(int n) {
+        int[] result = {0, 1};
+        if (n < 2) return result[n];
+
+        int fiOne = 0;
+        int fiTwo = 1;
+        int fiRes = 0;
+        for (int i = 2; i <= n; i++) {
+            fiRes = fiOne + fiTwo;
+            fiOne = fiTwo;
+            fiTwo = fiRes;
+        }
+        return fiRes;
+    }
 }
