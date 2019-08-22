4fc67729eda15b350e5b6254602667e1a2249810
DreamCats <maifeng_cat@outlook.com>
2019-08-22 15:27:16 +0800

    青蛙跳台阶问题

---
+++ SwordOffer/src/T10.java
@@ -0,0 +1,16 @@
+/**
+ * @program JavaBooks
+ * @description: 青蛙跳台阶问题
+ * @author: mf
+ * @create: 2019/08/22 15:25
+ */
+
+/*
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶
+求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+public class T10 {
+    public static void main(String[] args) {
+
+    }
+}
