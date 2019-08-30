db08feda9be46d9c846efbbbb6c6bec454eae248
DreamCats <maifeng_cat@outlook.com>
2019-08-30 09:54:58 +0800

    ListNode公共类

---
+++ SwordOffer/src/ListNode.java
@@ -0,0 +1,15 @@
+/**
+ * @program JavaBooks
+ * @description: 链表
+ * @author: mf
+ * @create: 2019/08/30 09:51
+ */
+
+public class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+}
