6cad17ba20a61f9f504950ac5c132cf25d215225
DreamCats <maifeng_cat@outlook.com>
2019-09-06 10:06:02 +0800

    抽出二叉树

---
+++ SwordOffer/src/TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ * @program JavaBooks
+ * @description: 二叉树
+ * @author: mf
+ * @create: 2019/09/06 10:05
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
