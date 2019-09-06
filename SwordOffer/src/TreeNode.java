96c225d7628a3b3d4018cdf3402e58a90753a54f
DreamCats <maifeng_cat@outlook.com>
2019-09-06 10:26:36 +0800

    重建二叉树方法

--- SwordOffer/src/TreeNode.java
+++ SwordOffer/src/TreeNode.java
@@ -13,4 +13,25 @@ public class TreeNode {
     public TreeNode(int val) {
         this.val = val;
     }
+
+    // 给定一个前序和中序数组，生成一颗二叉树  // 根据T7笔试题
+    public static TreeNode setBinaryTree(int[] pre, int[] in) {
+        TreeNode root = reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
+        return root;
+    }
+
+    private static TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
+
+        if (startPre > endPre || startIn > endIn) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[startPre]);
+        for (int i = startIn; i <= endIn; i++) {
+            if (in[i] == pre[startPre]) {
+                root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn, in, startIn, i - 1);
+                root.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+            }
+        }
+        return root;
+    }
 }
