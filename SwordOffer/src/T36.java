50bb4ea006c2c69c10150cfcd743286d64dc84bf
DreamCats <maifeng_cat@outlook.com>
2019-10-17 17:24:24 +0800

    update

--- SwordOffer/src/T36.java
+++ SwordOffer/src/T36.java
@@ -9,7 +9,7 @@
 输入一颗二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
 要求不能创建任何新的结点，只能调整树中结点指针的指向。
  */
-public class T36 {
+public class T36  {
     public static void main(String[] args) {
         int[] pre = {10, 6, 4, 8, 14, 12, 16};
         int[] in = {4, 6, 8, 10, 12, 14, 16};
