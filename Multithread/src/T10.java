33c2106c9fcfa93dc68e5ab24b1e6b03b409bc0f
DreamCats <maifeng_cat@outlook.com>
2019-12-29 23:24:14 +0800

    不要以字符串常量作为锁定对象

---
+++ Multithread/src/T10.java
@@ -0,0 +1,27 @@
+/**
+ * @program JavaBooks
+ * @description: 不要以字符串常量作为锁定对象
+ * @author: mf
+ * @create: 2019/12/29 00:34
+ */
+
+public class T10 {
+
+    private String s1 = "hello";
+
+    private String s2 = "hello";
+
+    void m1() {
+        synchronized (s1) {
+
+        }
+    }
+
+    void m2() {
+        synchronized (s2) {
+
+        }
+    }
+
+    // m1和m2锁的同一个字符串常量对象。
+}
