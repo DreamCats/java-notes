cda7161c63e4ad4022d799c3e5e4573349d88bbe
DreamCats <maifeng_cat@outlook.com>
2019-12-30 16:02:34 +0800

    线程通信，两个线程去操作容器的需求。。

---
+++ Multithread/src/T11.java
@@ -0,0 +1,55 @@
+/**
+ * @program JavaBooks
+ * @description: 通信
+ * @author: mf
+ * @create: 2019/12/30 15:54
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 实现一个容器，提供两个方法add size
+ * 写两个线程，线程1添加10个元素到容器中，线程2实现监控元素的个数，当个数到5个时，线程2给出提示并结束
+ */
+
+public class T11 {
+
+    List lists = new ArrayList();
+
+    public void add(Object o) {
+        lists.add(o);
+    }
+
+    public int size() {
+        return lists.size();
+    }
+
+    public static void main(String[] args) {
+        T11 t11 = new T11();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                t11.add(new Object());
+                System.out.println("add " + i);
+            }
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t1").start();
+
+
+        new Thread(() -> {
+            while (true) {
+                if (t11.size() == 5) {
+                    break;
+                }
+            }
+            System.out.println("t2结束");
+        }, "t2").start();
+    }
+}
