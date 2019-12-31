d361b9068450bd926ee57a4becfe927c58bec3e9
DreamCats <maifeng_cat@outlook.com>
2019-12-31 18:56:35 +0800

    使用vector

--- Multithread/src/T19.java
+++ Multithread/src/T19.java
@@ -7,6 +7,8 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 有N张火车票，每张票都有一个编号
@@ -30,6 +32,37 @@ public class T19 {
                 while (tickets.size() > 0) {
                     // size和remove不是原子性的
                     System.out.println("销售了--" + tickets.remove(0));
+                }
+            }).start();
+        }
+    }
+}
+
+/**
+ * 用vector安全容器
+ */
+class T19_1 {
+
+    private static Vector<String> lists = new Vector<>();
+
+    static {
+        for (int i = 0; i < 1000; i++) {
+            lists.add("编号：" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                while (lists.size() > 0) {
+                    // 加点延迟
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // 但是size和remove之间不是原子性的
+                    System.out.println("销售了--" + lists.remove(0));
                 }
             }).start();
         }
