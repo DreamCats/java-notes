beff38936b6864ba88c168afddd6ad85e0010b36
DreamCats <maifeng_cat@outlook.com>
2019-12-31 18:39:29 +0800

    模拟售票程序

---
+++ Multithread/src/T19.java
@@ -0,0 +1,36 @@
+/**
+ * @program JavaBooks
+ * @description: 售票程序
+ * @author: mf
+ * @create: 2019/12/31 18:36
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 有N张火车票，每张票都有一个编号
+ * 同时有10个窗口对外售票
+ *
+ */
+public class T19 {
+
+    private static List<String> tickets = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10000; i++) {
+            tickets.add("票编号：" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 10个窗口售票
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                while (tickets.size() > 0) {
+                    System.out.println("销售了--" + tickets.remove(0));
+                }
+            }).start();
+        }
+    }
+}
