7f60ba527cc1af3755dc0beb67df6afdeb8e660d
DreamCats <maifeng_cat@outlook.com>
2020-01-23 16:36:18 +0800

    Lambda表达式 例子

--- Basics/src/com/stream/LambdaDemo.java
+++ Basics/src/com/stream/LambdaDemo.java
@@ -7,6 +7,27 @@
 
 package com.stream;
 
-public class LambdaDemo {
+import java.util.*;
 
+public class LambdaDemo {
+    public static void main(String[] args) {
+        // 老版本的排序
+        List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+        System.out.println(names);
+//        Collections.sort(names, new Comparator<String>() {
+//            @Override
+//            public int compare(String a, String b) {
+//                return b.compareTo(a);
+//            }
+//        });
+
+        // 新版本 lambda表达式：
+
+//        Collections.sort(names, (String a, String b) -> {
+//            return b.compareTo(a);
+//        });
+//        Collections.sort(names, (String a, String b) -> b.compareTo(a));
+        names.sort((String a, String b) -> b.compareTo(a));
+        System.out.println(names);
+    }
 }
