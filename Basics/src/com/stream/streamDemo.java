11ea25f9397c1e47a038ee49583c3e153ae7e7d2
DreamCats <maifeng_cat@outlook.com>
2020-01-24 11:57:34 +0800

    java8的stream

---
+++ Basics/src/com/stream/streamDemo.java
@@ -0,0 +1,24 @@
+/**
+ * @program JavaBooks
+ * @description: 流的一些例子
+ * @author: mf
+ * @create: 2020/01/23 17:39
+ */
+
+package com.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class streamDemo {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("ddd", "bbb", "ccc", "www", "eee", "ddd2");
+
+        // 过滤
+        strings
+                .stream()
+                .filter((s) -> s.startsWith("d"))
+                .forEach(System.out::println);
+    }
+}
