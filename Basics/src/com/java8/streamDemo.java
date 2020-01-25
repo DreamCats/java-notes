709679020bb5671cffd3b69509de8b2cab6bbbd0
DreamCats <maifeng_cat@outlook.com>
2020-01-25 12:40:17 +0800

    stream

--- Basics/src/com/java8/streamDemo.java
+++ Basics/src/com/java8/streamDemo.java
@@ -9,6 +9,8 @@ package com.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class streamDemo {
     public static void main(String[] args) {
@@ -61,5 +63,28 @@ public class streamDemo {
                 .filter((s) -> s.startsWith("d"))
                 .count();
         System.out.println(count);
+
+        //规约
+        System.out.println("规约");
+        Optional<String> reduce = strings
+                .stream()
+                .sorted()
+                .reduce((s1, s2) -> s1 + "#" + s2);
+        reduce.ifPresent(System.out::println);
+
+        // 字符串拼接
+        String reduce1 = Stream.of("A", "B", "C").reduce("", String::concat);
+        System.out.println(reduce1);
+        // 求最小值
+        Double reduce2 = Stream.of(-1.5, 1.0, -3.0).reduce(Double.MAX_VALUE, Double::min);
+        System.out.println(reduce2);
+        // 求和
+        Integer reduce3 = Stream.of(1, 2, 3, 4).reduce(0, Integer::sum);
+        System.out.println(reduce3);
+        // 求和
+        Optional<Integer> reduce4 = Stream.of(1, 2, 3, 4).reduce(Integer::sum);
+        System.out.println(reduce4.get());
+
+
     }
 }
