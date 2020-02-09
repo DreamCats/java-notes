3ad3349c8eab29bf67436ff64ab83e4e1c4c153d
DreamCats <maifeng_cat@outlook.com>
2020-02-09 17:27:58 +0800

    添加student的构造方法

--- Basics/src/com/reflect/Student.java
+++ Basics/src/com/reflect/Student.java
@@ -13,6 +13,37 @@ public class Student {
 
     private Integer age;
 
+    // 无参数的构造方法
+    public Student() {
+        System.out.println("调用了公有、无参构造方法执行了。。。");
+    }
+
+    // 默认的构造方法
+    public Student(String str) {
+        System.out.println("(默认)的构造方法 s = " + str);
+    }
+
+    // 有一个参数的构造方法
+    public Student(char name) {
+        System.out.println("姓名：" + name);
+    }
+
+    // 有多个参数的构造方法
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //受保护的构造方法
+    protected Student(boolean n){
+        System.out.println("受保护的构造方法 n = " + n);
+    }
+
+    //私有构造方法
+    private Student(int age){
+        System.out.println("私有的构造方法   年龄："+ age);
+    }
+
 
     public String getName() {
         return name;
