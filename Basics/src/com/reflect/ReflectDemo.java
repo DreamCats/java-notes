9b786f0a1fdb19188879d01f23476f206ca2c91f
DreamCats <maifeng_cat@outlook.com>
2020-02-09 16:59:48 +0800

    第一种方式获取Class对象 new对象

--- Basics/src/com/reflect/ReflectDemo.java
+++ Basics/src/com/reflect/ReflectDemo.java
@@ -7,6 +7,18 @@
 
 package com.reflect;
 
+/**
+ * 获取Class对象的三种方式
+ * Object ——> getClass();
+ * 任何数据类型（包括基本数据类型）都有一个“静态”的class属性
+ * 通过Class类的静态方法：forName（String  className）(常用)
+ */
 public class ReflectDemo {
+    public static void main(String[] args) {
+        // 第一种方式获取Class对象
+        Student student = new Student(); // 这一new 产生一个Student对象，一个Class对象。
+        Class studentClass = student.getClass();
+        System.out.println(studentClass.getName()); // com.reflect.Student
 
+    }
 }
