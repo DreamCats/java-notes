31b620bb92433158625fce258126779742da3516
DreamCats <maifeng_cat@outlook.com>
2020-02-09 23:52:42 +0800

    反射，访问并调用方法

--- Basics/src/com/reflect/Student.java
+++ Basics/src/com/reflect/Student.java
@@ -71,6 +71,21 @@ public class Student {
         this.gender = gender;
     }
 
+    //**************成员方法***************//
+    public void show1(String s){
+        System.out.println("调用了：公有的，String参数的show1(): s = " + s);
+    }
+    protected void show2(){
+        System.out.println("调用了：受保护的，无参的show2()");
+    }
+    void show3(){
+        System.out.println("调用了：默认的，无参的show3()");
+    }
+    private String show4(int age){
+        System.out.println("调用了，私有的，并且有返回值的，int参数的show4(): age = " + age);
+        return "abcd";
+    }
+
     @Override
     public String toString() {
         return "Student{" +
