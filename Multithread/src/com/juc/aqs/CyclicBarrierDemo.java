7e0f6fc0a192d6cb2f7028323cf3632df30259cd
DreamCats <maifeng_cat@outlook.com>
2020-02-15 17:30:24 +0800

    CyclicBarrierDemo

---
+++ Multithread/src/com/juc/aqs/CyclicBarrierDemo.java
@@ -0,0 +1,36 @@
+/**
+ * @program JavaBooks
+ * @description: CyclicBarrierDemo
+ * @author: mf
+ * @create: 2020/02/15 17:21
+ */
+
+package com.juc.aqs;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        cyclicBarrierTest();
+    }
+
+    public static void cyclicBarrierTest() {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println("====召唤神龙====");
+        });
+        for (int i = 0; i < 7; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + " 收集到第" + tempInt + "颗龙珠");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }, "" + i).start();
+        }
+    }
+}
