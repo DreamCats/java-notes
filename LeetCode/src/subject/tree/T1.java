3d449776b2c1475f9d92a33d473987f062998e83
DreamCats <maifeng_cat@outlook.com>
2020-03-09 13:28:06 +0800

    相同的树

--- LeetCode/src/subject/tree/T1.java
+++ LeetCode/src/subject/tree/T1.java
@@ -26,6 +26,6 @@ public class T1 {
         if (p == null && q == null) return true; // 递归到最后二者都是null 说明相同
         if (p == null || q == null) return false; // 递归到其中有一方为空，则不相同
         if (p.val != q.val) return false; // 值不相等
-        return (p.left == q.left) && (p.right == q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
