681fc221919ae40eeff0f12d2b7830f8ba216727
DreamCats <maifeng_cat@outlook.com>
2020-04-09 17:30:04 +0800

    环形链表

---
+++ LeetCode/src/subject/dpointer/ListNode.java
@@ -0,0 +1,17 @@
+/**
+ * @program JavaBooks
+ * @description: ListNode
+ * @author: mf
+ * @create: 2020/03/06 18:39
+ */
+
+package subject.dpointer;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
