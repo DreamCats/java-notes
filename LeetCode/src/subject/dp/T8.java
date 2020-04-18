5209993e10b529058242cc95940c89bb86b80989
DreamCats <maifeng_cat@outlook.com>
2020-04-18 13:51:56 +0800

    最大乘积子序列

--- LeetCode/src/subject/dp/T8.java
+++ LeetCode/src/subject/dp/T8.java
@@ -7,6 +7,8 @@
 
 package subject.dp;
 
+import java.util.Arrays;
+
 /**
  *
  */
@@ -16,22 +18,25 @@ public class T8 {
         //返回值，用来存放阶段性最大值
         int res = Integer.MIN_VALUE;
         //创建两个数组，分别存放阶段性最大值和最小值的累乘
-        int[] dp_max = new int[nums.length+1];
-        int[] dp_min = new int[nums.length+1];
-        dp_max[0] = 1;
-        dp_min[0] = 1;
+        int[] dpMax = new int[nums.length+1];
+        int[] dpMin = new int[nums.length+1];
+        dpMax[0] = 1;
+        dpMin[0] = 1;
         //dp的循环
         for(int i=1; i<=nums.length; i++){
             //如果当前的nums[i-1]小于0，那么
             if(nums[i-1]<0){
-                int temp = dp_max[i-1];
-                dp_max[i-1] = dp_min[i-1];
-                dp_min[i-1] = temp;
+                int temp = dpMax[i-1];
+                dpMax[i-1] = dpMin[i-1];
+                dpMin[i-1] = temp;
             }
-            dp_max[i] = Math.max(dp_max[i-1]*nums[i-1], nums[i-1]);
-            dp_min[i] = Math.min(dp_min[i-1]*nums[i-1], nums[i-1]);
-            res = Math.max(res, dp_max[i]);
+            dpMax[i] = Math.max(dpMax[i-1]*nums[i-1], nums[i-1]);
+            dpMin[i] = Math.min(dpMin[i-1]*nums[i-1], nums[i-1]);
+            res = Math.max(res, dpMax[i]);
         }
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(dpMax));
+        System.out.println(Arrays.toString(dpMin));
         return res;
     }
 
