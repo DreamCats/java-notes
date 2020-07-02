85e4fda6e2365cfdffff7106715d1008ff6f910d
DreamCats <maifeng868@gmail.com>
2020-07-02 22:20:37 +0800

    网易面经一部分

--- Interview/src/Test/Test.java
+++ Interview/src/Test/Test.java
@@ -7,10 +7,27 @@
 
 package Test;
 
+import java.util.ArrayList;
+
 public class Test {
 
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
 
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        //
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i) == 2) list.remove(list.get(i));
+//        }
+        for (Integer integer : list) {
+            if (integer == 2) list.remove(integer);
+        }
+        System.out.println(list);
     }
 
 
