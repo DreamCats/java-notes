37caf1d872f0a98d7e965356a7216692cb6f5962
DreamCats <maifeng868@gmail.com>
2020-07-14 18:44:05 +0800

    招银二面

--- Interview/src/Test/Test.java
+++ Interview/src/Test/Test.java
@@ -7,12 +7,25 @@
 
 package Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
     public static void main(String[] args) {
-        int[] a = {1,2, 3};
+        int[] a = new int[]{1,2, 3};
 
         int[] b = new int[3];
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+
+        int[] c = list.stream().mapToInt(Integer::valueOf).toArray();
+        for (int i : c) {
+            System.out.println(i);
+        }
+
     }
 
 
