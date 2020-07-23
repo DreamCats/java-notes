c266f0aae439e134f64b9538f3170038783d4f7d
DreamCats <maifeng868@gmail.com>
2020-07-23 20:43:57 +0800

    个人字节面经-新

--- Interview/src/Test/Test.java
+++ Interview/src/Test/Test.java
@@ -1,35 +1,7 @@
-/**
- * @program JavaBooks
- * @description: 测试
- * @author: mf
- * @create: 2020/05/24 00:04
- */
-
 package Test;
 
-import java.util.Scanner;
-
 public class Test {
-
-    public static void main(String[] args) throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        s += s;
-        int cur = 1, max = 0;
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) != s.charAt(i + 1)){
-                cur++;
-            } else{
-                max = Math.max(max, cur);
-                cur = 1;
-            }
-        }
-        if (cur == s.length())
-            System.out.println(max / 2);
-        else
-            System.out.println(max);
+    public static void main(String[] args) {
+        System.out.println(12 % 10);
     }
-
-
-
 }
