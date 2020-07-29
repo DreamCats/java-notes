ebe3ff1f19d478daafacb85158359fc15c896f30
DreamCats <maifeng868@gmail.com>
2020-07-29 09:29:05 +0800

    Fibonacci数列

--- Interview/src/Test/Main.java
+++ Interview/src/Test/Main.java
@@ -7,25 +7,28 @@
 
 package Test;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+        if (n <= 2) System.out.println(0);
+        else {
+            int pre2 = 0, pre1 = 1;
+            for (int i = 3; i < n; i++) {
+                int sum = pre2 + pre1;
+                if (sum == n){
+                    System.out.println(0);
+                    break;
+                }
+                else if (pre1 < n && n < sum){
+                    System.out.println(Math.min(n - pre1, sum - n));
+                    break;
+                }
+                pre2 = pre1;
+                pre1 = sum;
+            }
         }
-        Arrays.sort(a);
-        int p = 1;
-        for (int i = 0; i < n; i++) {
-            if (a[i] > p)
-                break;
-            else
-                p += a[i];
-        }
-        System.out.println(p);
     }
 }
