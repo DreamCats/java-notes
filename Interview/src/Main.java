ea73d34a233fa5c56c62acbbf68024aabe317f0d
DreamCats <maifeng868@gmail.com>
2020-08-26 10:24:48 +0800

    shein 二面

--- Interview/src/Main.java
+++ Interview/src/Main.java
@@ -1,10 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> a = new ArrayList<>();
-        List<String> b = new ArrayList<>();
-        System.out.println(a.getClass() == b.getClass());
+    public int[][] merge(int[][] intervals) {
+        StringBuilder sb = new StringBuilder();
+        sb.cha
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){val = x;}
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
     }
 }
